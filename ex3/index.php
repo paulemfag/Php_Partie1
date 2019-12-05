a94712d094638c61f3eb832e5f6633b99df953ca
paulemfag <boiteprofagot@gmail.com>
2019-12-05 16:56:06 +0100

    correction

--- ex3/index.php
+++ ex3/index.php
@@ -1,46 +1,15 @@
-<!DOCTYPE html>
-<html lang="fr" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Php partie 1</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  </head>
-  <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item">
-        <a class="nav-link" href="http://phppartie1ex1.info">Exercice 1</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="http://phppartie1ex2.info">Exercice 2</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="http://phppartie1ex3.info">Exercice 3</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="http://phppartie1ex4.info">Exercice 4</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="http://phppartie1ex5.info">Exercice 5</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="http://phppartie1ex6.info">Exercice 6</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="http://phppartie1ex7.info">Exercice 7</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="http://phppartie1ex8.info">Exercice 8</a>
-      </li>
-    </ul>
-  </div>
-</nav>
 <?php
+$page = 'Exercice 3';
+include '../header.php';
 $km = 1;
-$km = 3;
-$km = 125;
-echo $km;
 ?>
-  </body>
-</html>
+<p>Valeur initiale de la variable kilomètre : <?= $km ?></p>
+<?php
+$km = 3;
+?>
+<p>Première modification de la valeur de la variable kilomètre : <?= $km ?></p>
+<?php
+$km = 125;
+?>
+<p>Dernière modification de la valeur de la variable kilomètre : <?= $km ?></p>
+<?php include '../footer.php'; ?>
