d5adc0d6f2e54f9c989aefddb47af2448116bdc5
paulemfag <boiteprofagot@gmail.com>
2019-12-05 14:45:50 +0100

    *

---
+++ ex8/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Php partie 1</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="http://phppartie1ex1.info">Exercice 1</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="http://phppartie1ex2.info">Exercice 2</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="http://phppartie1ex3.info">Exercice 3</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="http://phppartie1ex4.info">Exercice 4</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="http://phppartie1ex5.info">Exercice 5</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="http://phppartie1ex6.info">Exercice 6</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="http://phppartie1ex7.info">Exercice 7</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="http://phppartie1ex8.info">Exercice 8</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+<?php
+$first = 3 + 4;
+$second = 5 * 20;
+$third = 45 / 5;
+echo "le résultat de 3 plus 4 est $first, le résultat de 5 fois 20 est $second, le résultat de 45 divisé par 5 est $third";
+?>
+  </body>
+</html>
